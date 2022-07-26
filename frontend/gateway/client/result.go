5f1f30ed7af5a76963341b8ccdc9f2a88546bee2
Justin Chadwell <me@jedevc.com>
2022-07-26 10:25:47 +0100

    gateway: plumb attestations support through
    This patch connects the raw protobuf attestations through to the gateway
    clients and servers, ensuring that the results are properly passed
    through correctly, and that the appropriate refs are correctly cleaned
    up.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- frontend/gateway/client/result.go
+++ frontend/gateway/client/result.go
@@ -4,16 +4,31 @@ import (
 	"context"
 	"sync"
 
+	"github.com/moby/buildkit/util/attestation"
 	"github.com/pkg/errors"
 )
 
 type BuildFunc func(context.Context, Client) (*Result, error)
 
+type Attestation interface {
+	isClientAttestation()
+}
+
+type InTotoAttestation struct {
+	PredicateType string
+	PredicateRef  Reference
+	PredicatePath string
+	Subjects      []attestation.InTotoSubject
+}
+
+func (a *InTotoAttestation) isClientAttestation() {}
+
 type Result struct {
-	mu       sync.Mutex
-	Ref      Reference
-	Refs     map[string]Reference
-	Metadata map[string][]byte
+	mu           sync.Mutex
+	Ref          Reference
+	Refs         map[string]Reference
+	Metadata     map[string][]byte
+	Attestations map[string][]Attestation
 }
 
 func NewResult() *Result {
@@ -22,20 +37,29 @@ func NewResult() *Result {
 
 func (r *Result) AddMeta(k string, v []byte) {
 	r.mu.Lock()
+	defer r.mu.Unlock()
 	if r.Metadata == nil {
 		r.Metadata = map[string][]byte{}
 	}
 	r.Metadata[k] = v
-	r.mu.Unlock()
 }
 
 func (r *Result) AddRef(k string, ref Reference) {
 	r.mu.Lock()
+	defer r.mu.Unlock()
 	if r.Refs == nil {
 		r.Refs = map[string]Reference{}
 	}
 	r.Refs[k] = ref
-	r.mu.Unlock()
+}
+
+func (r *Result) AddAttestation(k string, v Attestation) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if r.Attestations == nil {
+		r.Attestations = map[string][]Attestation{}
+	}
+	r.Attestations[k] = append(r.Attestations[k], v)
 }
 
 func (r *Result) SetRef(ref Reference) {
