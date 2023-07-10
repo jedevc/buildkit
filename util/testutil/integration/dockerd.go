29241762b545126ff121b33ef9f05f0eef8b2764
Justin Chadwell <me@jedevc.com>
2023-07-10 16:59:07 +0100

    tests: set --iptables=false for dockerd tests
    Without this option, all tests will attempt to launch a daemon that all
    attempt to modify the same iptables chain which will fail.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- util/testutil/integration/dockerd.go
+++ util/testutil/integration/dockerd.go
@@ -132,6 +132,7 @@ func (c moby) New(ctx context.Context, cfg *BackendConfig) (b Backend, cl func()
 
 	err = d.StartWithError(cfg.Logs,
 		"--config-file", dockerdConfigFile,
+		"--iptables=false",
 		"--userland-proxy=false",
 		"--bip", "10.66.66.1/24",
 		"--default-address-pool", "base=10.66.66.0/16,size=24",
